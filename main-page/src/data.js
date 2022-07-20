8afc6a9579936b669d009a98d1523bbd942bedab
Finng1337 <cermak10@email.cz>
2022-07-20 13:29:07 +0200

    Typo in image url

--- main-page/src/data.js
+++ main-page/src/data.js
@@ -2,7 +2,7 @@ export default [
     {
         title: "Preview of services for car-sharing company",
         description: "Info about services with interactive transition on buttons. Component is fully responsive and was done using pure HTML and CSS. For layout was used flexbox.",
-        imageUrl: "https://raw.githubusercontent.com/finng1337/web-projects/main/car-sharing/images/project-cars.png",
+        imageUrl: "https://raw.githubusercontent.com/finng1337/web-projects/main/car-sharing/images/preview.png",
         siteURL: "https://finng1337.github.io/web-projects/car-sharing/",
         primaryColor: "hsl(0, 0%, 95%)",
         secondaryColor: "black"
@@ -10,7 +10,7 @@ export default [
     {
         title: "NFT card",
         description: "NFT card with shading and interactive transition on preview of the NFT. Component was done using pure HTML and CSS. I used grid as a main layout and flexbox for minor things.",
-        imageUrl: "https://raw.githubusercontent.com/finng1337/web-projects/main/nft-card/images/project-nft.png",
+        imageUrl: "https://raw.githubusercontent.com/finng1337/web-projects/main/nft-card/images/preview.png",
         siteURL: "https://finng1337.github.io/web-projects/nft-card/",
         primaryColor: "hsl(217, 54%, 11%)",
         secondaryColor: "white"
@@ -18,7 +18,7 @@ export default [
     {
         title: "Demo of time tracking app",
         description: "Time tracking demo with selecting time interval. Component is fully responsive and was done using pure HTML, CSS and vanilla javascript. I used grid as a main layout and flexbox for minor things. Data are fetched from json file.",
-        imageUrl: "https://raw.githubusercontent.com/finng1337/web-projects/main/time-tracking/images/project-timeTracking.png",
+        imageUrl: "https://raw.githubusercontent.com/finng1337/web-projects/main/time-tracking/images/preview.png",
         siteURL: "https://finng1337.github.io/web-projects/time-tracking/",
         primaryColor: "hsl(226, 43%, 10%)",
         secondaryColor: "white"
