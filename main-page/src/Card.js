fdc81496c33fda383bf8637518e740e6a92637ef
Finng1337 <cermak10@email.cz>
2022-07-20 14:16:44 +0200

    Made responsive links

--- main-page/src/Card.js
+++ main-page/src/Card.js
@@ -1,20 +1,37 @@
+import React from "react";
+
 export default function Card(props) {
+
+    const[linkBg, setLinkBg] = React.useState(props.secondaryColor);
+    const[linkText, setLinkText] = React.useState(props.primaryColor);
+
     const cardStyle = {
         backgroundColor: props.primaryColor,
         color: props.secondaryColor
     };
 
     const linkStyle = {
-        backgroundColor: props.secondaryColor,
-        color: props.primaryColor
+        backgroundColor: linkBg,
+        color: linkText
+    };
+
+    function linkEnter() {
+        setLinkBg("transparent");
+        setLinkText(props.secondaryColor);
     }
+
+    function linkExit() {
+        setLinkBg(props.secondaryColor);
+        setLinkText(props.primaryColor);
+    }
+
     return (
         <article style={cardStyle}>
             <img src={props.imageUrl} alt={"Preview of project"} className={"card-preview"}/>
             <section className={"card-about"}>
                 <h2 className={"card-title"}>{props.title}</h2>
                 <p className={"card-location"}>{props.description}</p>
-                <a href={props.siteURL} className={"card-siteLink"} style={linkStyle}>View site</a>
+                <a href={props.siteURL} className={"card-siteLink"} style={linkStyle} onMouseEnter={linkEnter} onMouseOut={linkExit}>View site</a>
             </section>
         </article>
     );
