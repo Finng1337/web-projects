6ddb2a01a317405eef93e91488db3480a144d37a
Finng1337 <cermak10@email.cz>
2022-07-20 16:32:57 +0200

    Added new project on main page

--- main-page/src/data.js
+++ main-page/src/data.js
@@ -17,10 +17,18 @@ export default [
     },
     {
         title: "Demo of time tracking app",
-        description: "Time tracking demo with selecting time interval. Component is fully responsive and was done using pure HTML, CSS and vanilla javascript. I used grid as a main layout and flexbox for minor things. Data are fetched from json file.",
+        description: "Time tracking demo with selecting time interval. Component is fully responsive and was done using HTML, CSS and vanilla javascript. I used grid as a main layout and flexbox for minor things. Data are fetched from json file.",
         imageUrl: "https://raw.githubusercontent.com/finng1337/web-projects/main/time-tracking/images/preview.png",
         siteURL: "https://finng1337.github.io/web-projects/time-tracking/",
         primaryColor: "hsl(226, 43%, 10%)",
         secondaryColor: "white"
+    },
+    {
+        title: "Travel blog",
+        description: "Travel journal with info about every travel. Page is fully responsive and was done using React library. For layout was used grid and flexbox",
+        imageUrl: "https://raw.githubusercontent.com/finng1337/web-projects/main/travel-journal/src/img/preview.png",
+        siteURL: "https://finng1337.github.io/web-projects/travel-journal/build",
+        primaryColor: "#f55a5a",
+        secondaryColor: "black"
     }
 ]
